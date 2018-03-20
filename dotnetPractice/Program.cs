f55e894d4d8e2bdbb00a6fb2df0a12fce14f4811
linh309 <linhn309@gmail.com>
2018-03-20 23:06:27 +0700

    use Task.Run

--- dotnetPractice/Program.cs
+++ dotnetPractice/Program.cs
@@ -1,31 +1,35 @@
 ﻿using System;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace dotnetPractice
 {
     class Program
     {
         bool _done;
-        static void WriteY()
-        {
-            for (int i = 0; i <= 3000; i++)
-            {
-                Console.Write("Y");
-            }
-        }
-
-        static void WriteX()
+        static void WriteA()
         {
             for (int i = 0; i <= 1000; i++)
             {
-                Console.Write("X");
+                Console.Write("A");
+            }
+        }
+
+        static void WriteB()
+        {
+            for (int i = 0; i <= 3000; i++)
+            {
+                Console.Write("B");
             }
         }
 
         static void Main(string[] args)
         {
-            new Thread(WriteX).Start();
-            new Thread(WriteY).Start();
+            //new Thread(WriteX).Start();
+            //new Thread(WriteY).Start();
+
+            Task.Run(() => WriteA());
+            Task.Run(() => WriteB());
 
             Console.ReadLine();
         }
