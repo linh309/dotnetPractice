d5b9806d2e2b10f8383e24a33da1a7b447678850
Nguyen Van Linh <linhnv309@live.com>
2018-03-20 22:16:42 +0700

    learning multi-thread

--- dotnetPractice/Program.cs
+++ dotnetPractice/Program.cs
@@ -1,12 +1,45 @@
 ﻿using System;
+using System.Threading;
 
 namespace dotnetPractice
 {
     class Program
     {
+        bool _done;
+        static void WriteY()
+        {
+            for (int i = 0; i <= 3000; i++)
+            {
+                Console.Write("Y");
+            }
+        }
+
+        void Go()
+        {
+            if (!_done)
+            {
+                _done = true;
+                Console.WriteLine("DONE");
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Thread t = new Thread(WriteY);
+            //t.Start();
+            ////t.Join();
+            ////Console.WriteLine("End WriteY");
+
+            //for(int i = 0; i <= 1000; i++)
+            //{
+            //    Console.Write("X");
+            //}
+
+            Program p = new Program();
+            new Thread(p.Go).Start();
+            p.Go();
+
+
             Console.ReadLine();
         }
     }
